540197e23af11c9ed7357b6195e3d285179f3dcd
Irfan Tito Kurniawan <42381907+titoirfan@users.noreply.github.com>
2021-02-28 16:13:28 +0700

    Update soal-02.c

--- soal-02.c
+++ soal-02.c
@@ -1,33 +1,100 @@
-/** EL2208 Praktikum Pemecahan Masalah dengan C 2020/2021
-*   Modul               : 
-*   Soal                : 2
-*   Hari dan Tanggal    : 
-*   Nama (NIM)          : 
-*   Asisten (NIM)       : 
-*   Nama File           : soal-02.c
-*   Deskripsi           : Deskripsi file ini.
-*/
-
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-
-// return 1 jika hanya mengandung digit, else return 0
-int valid_digit(char *ip_str);
-
-// return 1 jika IP string-nya valid, else return 0
+#include <stdlib.h> 
+#define DELIM "."
+ 
+/* return 1 jika hanya mengandung digit, else return 0 */
+int valid_digit(char *ip_str)
+{
+    while (*ip_str) {
+        if (*ip_str >= '0' && *ip_str <= '9')
+            ++ip_str;
+        else
+            return 0;
+    }
+    return 1;
+}
+ 
+/* return 1 jika IP string-nya valid, else return 0 */
 int valid_ip(char *ip_str)
-    
-// Prosedur untuk Mencetak Kelas dari IP Address
-void class_ip(char *ip_str)
+{
+    int num, dots = 0;
+    char *ptr;
+ 
+    if (ip_str == NULL)
+        return 0;
+ 
+    ptr = strtok(ip_str, DELIM);
+ 
+    if (ptr == NULL)
+        return 0;
+ 
+    while (ptr) {
+ 
+        /* setelah parsing string, harus hanya mengandung digit */
+        if (!valid_digit(ptr))
+            return 0;
+ 
+        num = atoi(ptr);
+ 
+        /* mengecek apakah IP Valid */
+        if (num >= 1 && num <= 255) {
+            /* parse string yang tersisa */
+            ptr = strtok(NULL, DELIM);
+            if (ptr != NULL)
+                ++dots;
+        } else
+            return 0;
+    }
+    /* harus mengandung 3 '.' alias titik */
+    if (dots != 3)
+        return 0;
+    return 1;
+}
 
-int main() {
-    // Tulis kode anda di sini
+/* Prosedur untuk Mencetak Kelas dari IP Address */
+void class_ip(char *ip_str)
+{
+    int num = 0;
+    char *ptr;
+ 
+    ptr = strtok(ip_str, DELIM);
+ 
+    if (ip_str != DELIM) {
+        num = atoi(ptr);
+        /* Prosedur akan langsung mencetak kelas IP berdasar range angkanya */
+        if (num >= 1 && num <= 127) {
+			printf("Alamat IP Kelas A.\n");
+		}
+		else if  (num > 127 && num <= 191) {
+			printf("Alamat IP Kelas B.\n"); 
+		}
+		else if  (num > 191 && num <= 224) {
+			printf("Alamat IP Kelas C.\n");
+		}
+		else if  (num > 224 && num <= 239) {
+			printf("Alamat IP Kelas D.\n");
+		}
+		else { 
+			printf("Alamat IP Kelas E.\n");
+		}
+	}
+}
+
+int main()
+{
+    char ip[20]={0};
     
-    // Template print untuk membantu autograding
-    // printf("Alamat IP Kelas A.\n");
-    // printf("Alamat IP Valid\n");
-    // printf("Alamat IP Tidak Valid\n");
+    printf("Masukkan Alamat IP : ");
+    scanf("%s",ip);
     
+    if (valid_ip(ip) ==  1) { 
+		printf("Alamat IP Valid\n"); 
+		class_ip(ip);
+	}
+	else { 
+		printf("Alamat IP Tidak Valid\n"); 
+	}
     return 0;
+    
 }
